9177f1cd418d04cb72e8dcfce03632aeeedc0861
Maciej Górecki <maciekgorecki@icloud.com>
2020-08-15 16:42:06 +0200

    Fixed formatting

--- covid-mac/View/WindowController.swift
+++ covid-mac/View/WindowController.swift
@@ -5,18 +5,11 @@
 import Cocoa
 
 class WindowController: NSWindowController {
-
     override func windowDidLoad() {
         super.windowDidLoad()
-    
-        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
     }
     
     override func makeTouchBar() -> NSTouchBar? {
-        guard let vc = contentViewController as? CovidViewController else {
-            return nil
-        }
-        return vc.makeTouchBar()
+        (contentViewController as? CovidViewController)?.makeTouchBar()
     }
-
 }
